af8afe4a5a1ee02a20fa5cbf46d5749ff65d406d
MelleRx <70823240+MelleRx@users.noreply.github.com>
2021-12-24 11:31:38 +0300

    Update swiper.js

--- js/swiper.js
+++ js/swiper.js
@@ -1,6 +1,7 @@
 window.addEventListener('load', () => {
     var swiper = new Swiper(".mySwiper", {
         pagination: {
+            clicable: true,
             el: ".swiper-pagination",
             dynamicBullets: true,
         },
